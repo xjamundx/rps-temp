3dc5a52b7ee5a25e6588afb7fed54420fafaebd8
Jamund Ferguson <jamuferguson@paypal.com>
2014-10-01 22:51:22 -0700

    failing test: fetch challenges

--- tests/logic.js
+++ tests/logic.js
@@ -1,5 +1,12 @@
+var assert = require('assert');
+
 describe('tic-tac-toe logic', function() {
 	it('should exist', function() {
 		var logic = require('../lib/logic');
+	});
+	it('should allow me to fetch some possible challenges', function() {
+		var logic = require('../lib/logic');
+		var moves = logic.getChallenges();
+		assert.ok(moves);
 	});
 });

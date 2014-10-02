1462af34275337669b809dfb729f3b1ba448387f
Jamund Ferguson <jamuferguson@paypal.com>
2014-10-02 13:23:31 -0700

    failing test: challenges

--- tests/logic.js
+++ tests/logic.js
@@ -8,5 +8,13 @@ describe('tic-tac-toe logic', function() {
 		var logic = require('../lib/logic');
 		var moves = logic.getChallenges();
 		assert.ok(moves);
+		assert.ok(Array.isArray(moves), 'moves need to be a list');
+	});
+	it('should allow me to determine who won a challenge', function() {
+		var logic = require('../lib/logic');
+		var moves = logic.getChallenges();
+		var winner = logic.challenge(moves[0], moves[1]);
+		assert.ok(winner, 'winner does not exist');
+		assert.ok([moves[0], moves[1]].indexOf(winner) > -1, 'winner was not found among challengers');
 	});
 });

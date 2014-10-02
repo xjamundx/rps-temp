d438b74a0b5d1f4a0905592141d5fffc71dd7b21
Jamund Ferguson <jamuferguson@paypal.com>
2014-10-01 22:45:21 -0700

    first failing test

---
+++ README.md
@@ -0,0 +1,3 @@
+Rock Paper Scissors
+
+This is a simple rock paper scissors game.
---
+++ tests/logic.js
@@ -0,0 +1,5 @@
+describe('tic-tac-toe logic', function() {
+	it('should exist', function() {
+		var logic = require('../lib/logic');
+	});
+});

cb4a3b1d87efde7577ba644028c0bd179bdbc5e0
Jamund Ferguson <jamuferguson@paypal.com>
2014-10-07 00:24:09 -0700

    finalized the basic feature-set

--- index.js
+++ index.js
@@ -11,35 +11,101 @@ var gameElement = document.querySelector('[data-game]');
 
 // add the heading part
 var heading = document.createElement('h1');
-heading.textContent = "Rock, Paper, Scissors";
+heading.textContent = 'Rock, Paper, Scissors';
 gameElement.appendChild(heading);
 
 // add the arena part
 var arena = document.createElement('div');
 arena.className = 'arena';
 gameElement.appendChild(arena);
-arena.textContent = 'Choose a move to play!';
+var text = document.createElement('p');
+text.textContent = 'Choose a move to play!';
+text.className = 'text-challenge';
+arena.appendChild(text);
 
-// play a challenge
+/**
+ * Get arena class name based on results of the challenge.
+ * @param success
+ * @param tie
+ * @returns {string}
+ */
+
+function getArenaClassName(success, tie) {
+	var className =  'arena';
+	if (success) {
+		className += ' arena-win';
+	} else if (tie) {
+		className += ' arena-tie';
+	} else {
+		className += ' arena-loss';
+	}
+	return className;
+}
+
+/**
+ * Generate a results message from the response callback data.
+ * @param {Object} data
+ * @param {Game.Challenge} data.opponent
+ * @param {Boolean} data.tie
+ * @param {Boolean} data.success
+ * @returns {string}
+ */
+function getResultMessage(data) {
+	var msg = 'You lost to';
+	if (data.success) {
+		msg = 'You beat';
+	} else if (data.tie) {
+		msg = 'You tied';
+	}
+	msg += ' ' + data.opponent.name + '!';
+	return msg;
+}
+
+/**
+ * Handle challenge responses
+ * @param {Error} err
+ * @param {Object} data
+ * @param {Game.Challenge} data.opponent
+ * @param {Boolean} data.tie
+ * @param {Boolean} data.success
+ */
+function handleChallengeResponse(err, data) {
+
+	// check for server errors and other strange things
+	if (err) {
+		arena.textContent = 'Unable to retrieve results of challenge. Please try again.';
+		return;
+	}
+
+	// update the arena color based on the result
+	arena.className = getArenaClassName(data.success, data.tie);
+
+	// show a message based on the result
+	var msg = getResultMessage(data);
+	var existingText = document.querySelector('.text-challenge');
+	var p = existingText ? existingText : document.createElement('p');
+	p.textContent = msg;
+	if (!p) {
+		p.className = 'text-challenge';
+		arena.appendChild(p);
+	}
+
+	// update an existing image or add a new one
+	var existingImg = document.querySelector('.img-challenge');
+	var img = existingImg ? existingImg : new Image();
+	img.src = data.opponent.imageUrl;
+	if (!existingImg) {
+		img.className = 'img-challenge';
+		arena.appendChild(img);
+	}
+}
+
+/**
+ * Challenge the opponent.
+ * @param {Game.Challenge} challenge
+ */
 function playChallenge(challenge) {
-	game.challenge(challenge, function(err, data) {
-		if (err) {
-			arena.textContent = "Unable to retrieve results of challenge. Please try again.";
-			return;
-		}
-
-		// generate an interesting message
-		var msg = 'You lost to';
-		if (data.success) {
-			msg = 'You beat';
-		} else if (data.tie) {
-			msg = 'You tied';
-		}
-		msg + ' ' + data.opponent.name + '!';
-
-		// display said message
-		arena.textContent = msg;
-	});
+	game.challenge(challenge, handleChallengeResponse);
 }
 
 // provide the challenges
